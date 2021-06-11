c7fee2b26b561f706c6a2c2b02386980091d1b72
subinkim26 <dpaptlove@gmail.com>
2021-06-11 12:25:44 +0900

    자바 데이터 타입

---
+++ Data_and_operation/Datatype.java
@@ -0,0 +1,17 @@
+public class Datatype{
+	public static void main(String[] args) {
+		System.out.println(6);//number
+		System.out.println("six");//string
+		
+		System.out.println("6");//string
+		
+		System.out.println(6+6); // 12
+		System.out.println("6"+"6"); //66
+		
+		System.out.println(6*6); // 36
+		//Syste.out.println("6"*"6"); 문자열곱하기못함
+		
+		System.out.println("1111".length()); //4
+		//System.out.println(1111.length());
+	}
+}

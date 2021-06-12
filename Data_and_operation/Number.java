100ad502631d034aeeab630e2ec4e923b127c609
subinkim26 <dpaptlove@gmail.com>
2021-06-12 15:32:06 +0900

    데이터 연산, 문자열 다루기

---
+++ Data_and_operation/Number.java
@@ -0,0 +1,20 @@
+
+public class Number {
+
+	public static void main(String[] args) {
+		
+		//연산자
+		System.out.println(6+2); //8
+		System.out.println(6-2); // 4
+		System.out.println(6*2); //12
+		System.out.println(6/2); //3
+		
+		
+		
+		System.out.println(Math.PI);//파이
+		System.out.println(Math.floor(Math.PI));
+		System.out.println(Math.ceil(Math.PI));
+
+	}
+
+}
---
+++ Data_and_operation/StringApp.java
@@ -0,0 +1,20 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		
+		//Character VS String
+		System.out.println("Hello World");
+		System.out.println('H');
+		System.out.println("H");
+		
+		System.out.println("Hello"+"World");
+		
+		//New Line
+		System.out.println("Hello \nWorld");
+		
+		//Escape
+		System.out.println("Hello \"World\"");
+	}
+
+}

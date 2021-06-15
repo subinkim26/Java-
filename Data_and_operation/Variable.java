8dbd3d6036b8e456e2a7a705afee28abaeb432e0
subinkim26 <dpaptlove@gmail.com>
2021-06-15 11:31:46 +0900

    변수정의와 타입변환

---
+++ Data_and_operation/Variable.java
@@ -0,0 +1,18 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 1; // -2,1,0,2 .. 
+		System.out.println(a);
+		
+		double b = 1.1; //-2.0, 1.0, 3.5 .. .
+		System.out.println(b);
+		
+		String c = "Hello World"; 
+		System.out.println(c);
+
+	}
+
+}

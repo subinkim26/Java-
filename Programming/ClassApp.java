aac01506c6dc806b0519165565f78387ef83e364
subinkim26 <dpaptlove@gmail.com>
2021-06-21 12:37:15 +0900

    인스턴스, 클래스, 상속

---
+++ Programming/ClassApp.java
@@ -0,0 +1,14 @@
+import java.io.FileNotFoundException;
+
+public class ClassApp {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		System.out.println(Math.PI);
+		//소수점 버리고 출력
+		System.out.println(Math.floor(1.6));
+		//특정 소수점부터 반올림 출력
+		System.out.println(Math.ceil(1.6));
+
+	}
+
+}
